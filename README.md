b63a3a90fd1842004669ba572f7f94e6b9b29a49
MiPersona <bgva2005@yahoo.com>
2020-05-19 00:42:07 -0500

    005

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 002 -> Reusar un componente
 003 -> Compactar JavaScript para producción
 004 -> Prueba React con JSX
+005 -> Agregar JSX a un proyecto
---
+++ src/components/like_button_03.js
@@ -0,0 +1,6 @@
+// Muestra un <button> que contenga "Like"
+return (
+  <button onClick={() => this.setState({ liked: true })}>
+    Like
+  </button>
+);
